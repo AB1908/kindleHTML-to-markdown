85a20a715785f43a458b3b8540b63852f12ed78a
AB1908 <14124383+AB1908@users.noreply.github.com>
2021-09-14 08:01:13 +0530

    Refactor and improve note parsing
    - Edge cases handled appropriately
    - Note parsing refactored to be more readable

--- kindle2md.py
+++ kindle2md.py
@@ -2,16 +2,7 @@
 #
 # This script gets a HTML annotations file exported from the Kindle app
 # and convert it to markdown.
-#
-# Instructions:
-# 1. Download this script.
-# 2. Be sure to have python installed.
-# 3. Install python dependencies: pip3 install --user beautifulsoup4
-# 4. Run the script: python3 kindle2md YOUR_FILE.html
-#
-# Copyright (C) 2021 Rafael Cavalcanti - rafaelc.org
-# Licensed under GPLv3
-#
+
 
 from pathlib import Path
 from os.path import basename, splitext
@@ -37,7 +28,6 @@ if dest.exists():
         exit(1)
 
 try:
-    # Not providing encoding throws an error on Windows ("deferredreward")
     file_content = source.read_text(encoding='UTF-8')
 except OSError as e:
     print(f'Failed to read file: {e}.')
@@ -45,45 +35,53 @@ except OSError as e:
 
 soup = BeautifulSoup(file_content, 'html.parser')
 
+def consume_highlight(text_div, header_div, location_data):
+    color = header_div.span.text.title()
+    highlight_location = location_data[2].strip()
+    highlight_text = text_div.text.strip()
+    kindle_highlight = {"type": "highlight", "color": color, "text": highlight_text, "location": highlight_location, "notes": []}
+    return kindle_highlight
+
+def chapter_init(notes, location_data):
+    chapter_marker = location_data[0].strip()
+    notes.setdefault(chapter_marker,[])
+    return chapter_marker
+
+def create_new_note(notes, chapter_marker, kindle_highlight, location, note):
+    notes[chapter_marker].append(kindle_highlight)
+    kindle_highlight = {"type": "note", "text": note, "location": location}
+    return kindle_highlight
+
 try:
-    note_divs = soup.select("div")[6:]
+    note_divs = soup.findAll("div", {'class': ["noteHeading","noteText"]})
     book_title = soup.select_one('.bookTitle').contents[0].strip()
-    notes = {}
-    div_type = None
-    chapter = ""
+    kindle_notes = {}
+    chapter_marker = chapter_init(kindle_notes, note_divs[0].text.partition('-')[2].partition('>'))
     kindle_highlight = {}
     for i in range(len(note_divs)):
         elem = note_divs[i]
         if 'noteHeading' in elem.attrs['class']:
             note_headers = elem.text.partition('-')
             location_data = note_headers[2].partition('>')
+            note_location = location_data[2].strip()
             if elem.text.strip().startswith("Highlight"):
                 if kindle_highlight:
-                    notes[chapter].append(kindle_highlight)
-                    div_type = None
-                    chapter = ""
-                    kindle_highlight = {}
-                color = elem.span.text.title()
-                chapter = location_data[0].strip()
-                notes.setdefault(chapter,[])
-                location = location_data[2].strip()
-                div_type = note_headers[0].strip()
-                highlight_text = note_divs[i+1].text.strip()
-                kindle_highlight = {"type": "highlight", "color": color, "text": highlight_text, "location": location, "notes": []}
+                    kindle_notes[chapter_marker].append(kindle_highlight)
+                    chapter_marker = chapter_init(kindle_notes, location_data)
+                kindle_highlight = consume_highlight(note_divs[i+1], elem, location_data)
             else:
                 note_chapter = location_data[0].strip()
-                note = note_divs[i+1].text.strip()
-                if note_chapter != chapter:
-                    notes[chapter].append(kindle_highlight)
-                    kindle_highlight = {"type": "note", "text": note, "location": location}
-                    chapter = note_chapter
-                    notes.setdefault(chapter,[])
-                    notes[chapter].append(kindle_highlight)
+                note_text = note_divs[i+1].text.strip()
+                if note_chapter != chapter_marker:
+                    kindle_highlight = create_new_note(kindle_notes, chapter_marker, kindle_highlight, note_location, note_text)
+                    chapter_marker = chapter_init(kindle_notes, location_data)
                 elif kindle_highlight["type"] == "highlight":
-                    kindle_highlight["notes"].append(note)
+                    kindle_highlight["notes"].append(note_text)
+                elif kindle_highlight["type"] == "note":
+                    kindle_highlight = create_new_note(kindle_notes, chapter_marker, kindle_highlight, note_location, note_text)
                 else:
-                    notes[chapter].append(kindle_highlight)
-    notes[chapter].append(kindle_highlight)
+                    raise Exception
+    kindle_notes[chapter_marker].append(kindle_highlight)
 
 except AttributeError as e:
     print(f'Error parsing file: {e}')
@@ -91,22 +89,23 @@ except AttributeError as e:
 
 output = f'# {book_title}\n\n'
 pandoc_div = ":::"
-for chapter in notes:
-    output += f'## {chapter}\n\n'
-    for kindle_highlight in notes[chapter]:
+for chapter_marker in kindle_notes:
+    output += f'## {chapter_marker}\n\n'
+    for kindle_highlight in kindle_notes[chapter_marker]:
         entry = pandoc_div
         if kindle_highlight["type"] == "note":
-            entry += "\n"
-            entry += f'> {kindle_highlight["text"]}\n'
+            entry += 'standalone\n\n'
+            entry += f'Note: {kindle_highlight["text"]}\n\n'
         if kindle_highlight["type"] == "highlight":
             entry += f'{kindle_highlight["color"].lower()}\n\n'
             entry += f'> {kindle_highlight["text"]}\n\n'
-            for note in kindle_highlight["notes"]:
-                entry += f'{note}\n\n'
+            for note_text in kindle_highlight["notes"]:
+                entry += f'{note_text}\n\n'
         entry += pandoc_div + "\n"
         output += entry + "\n"
 
 try:
+    output = output.rstrip('\n')
     dest.write_text(output, encoding='UTF-8')
 except OSError as e:
     print(f'Failed to write file: {e}')
