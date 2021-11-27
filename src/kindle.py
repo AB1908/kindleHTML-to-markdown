991d1a3b425bd2dc37b6e58a08173128a42f9591
AB1908 <14124383+AB1908@users.noreply.github.com>
2021-11-28 00:06:46 +0530

    Add HighlightColor enum

--- src/kindle.py
+++ src/kindle.py
@@ -1,9 +1,38 @@
+from enum import Enum
+
+# TODO: Fix super() usage file wide
+
+class HighlightColor(Enum):
+
+    # Note that this is dependent on the app itself.
+    PINK = 3
+    ORANGE = 4
+    BLUE = 2
+    YELLOW = 1
+
+    def __eq__(color1, color2):
+        return color1.value == color2.value
+
+    @staticmethod
+    def get_color(color_text):
+        if color_text == "pink":
+            color = HighlightColor.PINK
+        elif color_text == "blue":
+            color = HighlightColor.BLUE
+        elif color_text == "yellow":
+            color = HighlightColor.YELLOW
+        elif color_text == "orange":
+            color = HighlightColor.ORANGE
+        else:
+            raise Exception("Undefined color")
+        return color
+
 class AnnotationObject:
 
     def __init__(self, chapter, location, text):
-        this.text = text
-        this.location = location
-        this.chapter = chapter
+        self.text = text
+        self.location = location
+        self.chapter = chapter
 
     # The object's format and type is unknown so this must be implemented in derived class.
     def export_as_markdown(self):
@@ -20,7 +49,7 @@ class KindleHighlights:
     # Pass input to this class which parses
     # Also keep a separate export method for each class?
     # Allow overriding?
-    kindle_highlight = {"type": "highlight", "color": color, "text": highlight_text, "location": highlight_location, "notes": []}
+    # TODO: parse highlights
 
     def create(self):
         pass
