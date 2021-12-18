6e058cc67d87d254011b6bbcb21e1a41be0a728e
AB1908 <14124383+AB1908@users.noreply.github.com>
2021-12-18 17:40:53 +0530

    Update test parameters and constants

--- tests/test_kindle.py
+++ tests/test_kindle.py
@@ -2,20 +2,20 @@
 
 import src.kindle as kindle
 
-filename = "sample.html"
+# TODO: Look into test rewrites to properly unit test
 
 def test_example_chapter(example_chapter, example_highlight, example_note):
-    test_chapter = kindle.Chapter("CHAPTER 5. I AM SENT AWAY FROM HOME")
+    test_chapter = kindle.Chapter(example_highlight().chapter)
     test_chapter.kindle_highlights.append(example_highlight())
     test_chapter.kindle_highlights.append(example_note())
     assert example_chapter() == test_chapter
 
-def test_create_highlight_from_div(test_note_divs, example_highlight):
-    actual_highlight = kindle.KindleHighlights().create_text_highlight(test_note_divs[0], test_note_divs[1])
+def test_create_highlight_from_div(test_note_divs, example_highlight, highlight_index):
+    actual_highlight = kindle.KindleHighlights().create_text_highlight(test_note_divs[highlight_index], test_note_divs[highlight_index+1])
     assert example_highlight() == actual_highlight
 
-def test_create_new_note_from_div(test_note_divs, example_note):
-    actual_note = kindle.KindleHighlights().create_new_note(test_note_divs[2], test_note_divs[3])
+def test_create_new_note_from_div(test_note_divs, example_note, note_index):
+    actual_note = kindle.KindleHighlights().create_new_note(test_note_divs[note_index], test_note_divs[note_index+1])
     assert example_note() == actual_note
 
 def test_highlight_with_notes_equality(example_highlight, example_note):
